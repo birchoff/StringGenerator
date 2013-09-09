9aa9c731a7e0cdd2712195acc3587cb730617e97
birchoff <birchoff@gmail.com>
2013-09-09 17:39:16 -0400

    Refactored string generation to avoid some pitfalls
    The original implementation of string generation made it difficult to
    handle generating all strings while maintaining the ability to randomly
    generate a string.

--- StringGenerator/Generate.fs
+++ StringGenerator/Generate.fs
@@ -2,6 +2,8 @@
     open RegularExpressionParser
     open System.Text
 
+    type StringGenerator = unit -> string option
+
     let asciiCharacters = seq{yield 9}|> Seq.append (seq{32 .. 126}) |> Seq.map (fun ch -> char ch) |> Seq.toList
 
     let permutationWithRepitionMap size numberOfElements =
@@ -23,72 +25,125 @@
 
     
 
-    let generateString length =
+    let generateStrings length =
         let appendCharToStringBuilder (ch:char) (sb:StringBuilder) = sb.Append(ch)
         permutationWithRepitionMap length asciiCharacters.Length
         |> Seq.map 
             (fun permMap -> 
                 permMap 
-                |> Array.fold 
-                    (fun sb charPos -> sb |> appendCharToStringBuilder (charPos - 1 |> List.nth asciiCharacters)) (new StringBuilder(permMap.Length))
+                |> Array.fold  (fun sb charPos -> 
+                    sb |> appendCharToStringBuilder (charPos - 1 |> List.nth asciiCharacters)) (new StringBuilder(permMap.Length)
+                )
+                |> fun sb -> sb.ToString()
             )
+        |> Seq.toArray
 
-    let regExToString regex =
-        let cloneStringBuilders sbs = sbs |> Seq.map (fun sb -> new StringBuilder(sb.ToString()))
-        let rec regExToString (sbs:StringBuilder seq) =
-            function
-            | RChar(ch) -> seq{for sb in sbs do yield sb.Append(ch)}
+    let randomStringGenerator (): StringGenerator =
+        let generatorState = ref (0,0)
+        let currentStrings: string array option ref = ref None
+        let nextString = function 
+            | (0,_) -> ("", (1,0))
+            | (size, currentStringIndex) as state -> 
+                match !currentStrings with
+                | None -> 
+                    let currentStrings' = generateStrings size
+                    currentStrings := currentStrings' |> Some
+                    (currentStringIndex |> Array.get currentStrings', state)
+                | Some(currentStrings') when currentStringIndex = (currentStrings' |> Array.length) - 1 ->
+                    currentStrings := None
+                    (currentStringIndex |> Array.get currentStrings', (size+1, 0))
+                | Some(currentStrings) -> 
+                    (currentStringIndex |> Array.get currentStrings, (size, currentStringIndex + 1))
+        fun () ->
+            let str, newState = !generatorState |> nextString
+            generatorState := newState
+            str |> Some
+            
 
-            |RConcat(regex1, regex2) -> regex2 |> regExToString (regex1 |> regExToString sbs)
-
-            | RStartsWith(regex') 
-            | REndsWith(regex') 
-            | RExact(regex') -> regExToString sbs regex'
-
-            | RUnion(regexL, regexR) -> 
-                let leftResult = regExToString sbs regexL
-                let rightResult = regExToString (sbs |> cloneStringBuilders) regexR
-                Seq.append leftResult rightResult
-
-            | RStar(regex') ->
-                (cloneStringBuilders sbs, RNone)
-                |> Seq.unfold 
-                        (
-                            fun state -> 
-                                let result =
-                                    match state with
-                                    | clonedSbs,RNone -> clonedSbs
-                                    | clonedSbs, regex'' -> regExToString clonedSbs regex''
-                                Some(result, (cloneStringBuilders result, regex'))
-                        )
-                |> Seq.collect (fun sbs -> sbs)
-                    
-
-            | RNone -> sbs
-        regExToString (seq{yield new StringBuilder()}) regex |> Seq.map (fun sb -> sb.ToString())
+    let rec regexToStringGenerator = function
+        | RAny as rany -> anyStringGenerator rany
+        | RChar(_) as rchar -> charStringGenerator rchar
+        | RStar(_) as rstar -> starStringGenerator rstar
+        | RPlus(_) as rplus -> plusStringGenerator rplus
+        | RGroup(_) as rgroup -> groupStringGenerator rgroup
+    and anyStringGenerator rany : StringGenerator = 
+        let currentStrings : string array option ref = ref None
+        let nextString = function 
+            | currentStringIndex as state -> 
+                match !currentStrings with
+                | None -> 
+                    let currentStrings' = generateStrings 1
+                    currentStrings := currentStrings' |> Some
+                    Some(currentStringIndex |> Array.get currentStrings', state)
+                | Some(currentStrings') when currentStringIndex = (currentStrings' |> Array.length) ->
+                    None
+                | Some(currentStrings) -> 
+                    Some(currentStringIndex |> Array.get currentStrings, state + 1)
+        let generatorState = ref 0
+        match rany with
+        | RAny ->
+            fun () ->
+                match !generatorState |> nextString with
+                | Some(str, newState) ->
+                    generatorState := newState
+                    str |> Some
+                | _ -> None
+        | _ -> fun () -> None
+    and charStringGenerator rchar : StringGenerator = 
+        match rchar with
+        | RChar(ch) -> fun () -> ch.ToString() |> Some
+        | _ -> fun () -> None
+    and starStringGenerator rstar : StringGenerator =
+        match rstar with
+        | RStar(regex) ->
+            let stringGenerator = regex |> regexToStringGenerator
+            let generatorState = ref 0
+            fun () ->
+                let sb = new StringBuilder()
+                let rec generate times (accum:StringBuilder) =
+                    if times > 0 then accum.Append(stringGenerator()) |> generate (times - 1)
+                    else accum
+                sb |> generate !generatorState |> ignore
+                incr generatorState
+                sb.ToString() |> Some
+        | _ -> fun () -> None
+    and plusStringGenerator rplus : StringGenerator =
+        match rplus with
+        | RPlus(regex) ->
+            let stringGenerator = regex |> regexToStringGenerator
+            let generatorState = ref 1
+            fun () ->
+                let sb = new StringBuilder()
+                let rec generate times (accum:StringBuilder) =
+                    if times > 0 then accum.Append(stringGenerator()) |> generate (times - 1)
+                    else accum
+                sb |> generate !generatorState |> ignore
+                incr generatorState
+                sb.ToString() |> Some
+        | _ -> fun () -> None
+    and groupStringGenerator rgroup : StringGenerator =
+        match rgroup with
+        | RGroup(regex) -> regex |> regexToStringGenerator
+        | _ -> fun() -> None
+    and startsWithStringGenerator rstartsWith : StringGenerator =
+        match rstartsWith with
+        | RStartsWith(regex) ->
+            let prefixStringGenerator = regexToStringGenerator regex
+            let postfixStringGenerator =  randomStringGenerator() |> ref
+            let currentPostfixStrings : string array option ref  = ref None
+            let currentPrefixString : string option ref = ref None
+            fun () -> None
+        | _ -> fun () -> None
 
     let matchingStringsFor regExp length =
         let parseResult = parse regExp
-        let regExStrings = regExToString parseResult
-        let helper (regExString:string) =
-            match length, parseResult, length - regExString.Length with
-
-            | _ when regExString.Length = 0 -> Seq.empty
-        
-            | _, _, difference when difference = 0 -> seq{yield regExString}
-
-            | _, RStartsWith(_), difference when difference >= 1 -> 
-                generateString difference |> Seq.map (fun sb -> sb.Insert(0, regExString).ToString())
-
-            | _, REndsWith(_), difference when difference >= 1 -> 
-                generateString difference |> Seq.map (fun sb -> sb.Append(regExString).ToString())
-
-            | _, RExact(_), difference when difference > 0 -> Seq.empty
-
-            | _, _, difference when difference >= 1 -> 
-                generateString difference
-                |> Seq.map (fun sb -> sb.ToString())
-                |> Seq.collect (fun str -> seq{for i in 0 .. str.Length-1 do yield str.Insert(i, regExString)})
-
-            | _ -> Seq.empty
-        regExStrings |> Seq.takeWhile (fun regExString -> regExString.Length <= length ) |> Seq.collect helper
+        let generator = regexToStringGenerator parseResult
+        generator
+        |> Seq.unfold (
+            fun generator ->
+                let str = generator()
+                match str with
+                | None -> None
+                | Some(str) -> Some(str, generator)
+        )
+        |> Seq.takeWhile (fun str -> str |> String.length <= length)
